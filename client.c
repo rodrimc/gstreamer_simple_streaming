0ad79fc2c198edfe561e8256a9ba0be346d9a73e
Rodrigo Costa <rodrigocosta@telemidia.puc-rio.br>
2015-08-24 15:57:17 -0300

    Another minor typo correction

--- client.c
+++ client.c
@@ -145,7 +145,7 @@ int main (int argc, char *argv[])
   
   if (!gst_element_link_many (app.source, app.buffer, app.decoder, NULL))
   {
-    fprintf (stderr, "Could not link elments: source --> buffer --> decoder\n"
+    fprintf (stderr, "Could not link elements: source --> buffer --> decoder\n"
           "Exiting...\n");
     return 0;
   }
